386559a279c7b6cbc4023965029019659eec6343
Malay Mehta <meetmalay@gmail.com>
2019-01-22 00:14:01 +0530

    - Corrected the Default Date

--- src/app/adddata/adddata.component.ts
+++ src/app/adddata/adddata.component.ts
@@ -20,7 +20,7 @@ export interface Country {
 export class AdddataComponent implements OnInit {
     form: FormGroup;
 
-    startDate = new Date(1990, 0, 1);
+    startDate = new Date(2019, 1, 20);
     selectedCountry: string;
     user_country: Country[] = [
         {country_id: '1', country_name: 'Germany'},
